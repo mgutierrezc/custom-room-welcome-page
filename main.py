09766377ce6112a0d1774204f16acc5050fd5221
Marco Gutierrez <a20141676@pucp.pe>
2021-06-24 20:32:39 -0500

    prepared for columbia domain | only debugging

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ labels = Path('labels.txt').read_text('utf8').split()
 
 print('labels:', labels)
 
-OTREE_SERVER = "http://localhost:8000"
+OTREE_SERVER = "http://cgr-bid.econ.columbia.edu"
 ROOM_NAME = 'test_room'
 
 
